24c1d4ae229b01b481fcc005166cc71b393bcff6
luckynguyen1995 <tg.luckynguyen.1995@gmail.com>
2017-03-27 00:53:17 +0700

    123
    123

---
+++ Cxulynhanvien.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thuctapdemoGiaoDien
+{
+    class Cxulynhanvien
+    {
+
+        private databaseDataContext dc = new databaseDataContext();
+
+        public IEnumerable<object> getDSNhanVien()
+        {
+            return dc.NhanViens.Select(x => new NhanVienView
+            {
+                MaNV=x.MaNV,
+                TenNV=x.TenNV,
+                NgaySinh=x.NgaySinh,
+                GioiTinh=x.GioiTinh?"Nam":"Nữ",
+                DiaChi=x.DiaChi,
+                SDT=x.SDT
+            });
+            
+        }
+        public string phatsinhma()
+        {
+            string so = "";
+            var kq = dc.NhanViens.OrderByDescending(x => x.MaNV).Take(1);
+            if (kq.Count() <= 0) so = "NV0001";
+            else
+            {
+                foreach (NhanVien a in kq.ToList())
+                {
+                    string s = a.MaNV.Substring(2);
+                    int n = int.Parse(s) + 1;
+                    so = "NV" + n.ToString("D4");
+
+                    break;
+                }
+            }
+            return so;
+        }
+        public void Them(NhanVien a)
+        {
+            dc.NhanViens.InsertOnSubmit(a);
+            dc.SubmitChanges();
+        }
+
+        public void Xoa(NhanVien a)
+        {
+            dc.NhanViens.DeleteOnSubmit(a);
+            dc.SubmitChanges();
+
+        }
+        public void Sua(NhanVien nv)
+        {
+            foreach (NhanVien a in dc.NhanViens.Where(x => x.MaNV == nv.MaNV))
+            {
+                a.TenNV = nv.TenNV;
+                a.GioiTinh = nv.GioiTinh;
+                a.NgaySinh = nv.NgaySinh;
+                a.DiaChi = nv.DiaChi;
+                a.SDT = nv.SDT;
+                dc.SubmitChanges();
+            }
+        }
+        public NhanVien Tim(NhanVien nv)
+        {
+            foreach (NhanVien a in dc.NhanViens.Where(x => x.MaNV == nv.MaNV))
+            {
+                if (a != null)
+                    return a;
+            }
+            return null;
+        }
+        public List<NhanVien> getDSNVTracuu(NhanVien a,string strMa,string strTen)
+        {
+            List<NhanVien> b = new List<NhanVien>();
+            if (strTen == "")
+            {
+                foreach (NhanVien r in dc.NhanViens.Where(x => x.MaNV.Contains(strMa)))
+                {
+
+                    NhanVien e = b.Find(x => x.MaNV == r.MaNV);
+                    if (e == null)
+                    {
+
+                        b.Add(r);
+
+                    }
+                }
+                return b;
+            }
+            else if (strMa == "")
+            {
+                foreach (NhanVien k in dc.NhanViens.Where(x => x.TenNV.Contains(strTen)))
+                {
+                    NhanVien e = b.Find(x => x.MaNV == k.MaNV);
+                    if (e != null)
+                    {
+                        b.Remove(e);
+
+                    }
+                    b.Add(k);
+                }
+                return b;
+            }
+            else
+            {
+                foreach (NhanVien r in dc.NhanViens.Where(x => x.MaNV.Contains(strMa)))
+                {
+
+                    NhanVien e = b.Find(x => x.MaNV == r.MaNV);
+                    if (e == null)
+                    {
+
+                        b.Add(r);
+
+                    }
+                }
+                foreach (NhanVien k in dc.NhanViens.Where(x => x.TenNV.Contains(strTen)))
+                {
+                    NhanVien e = b.Find(x => x.MaNV == k.MaNV);
+                    if (e != null)
+                    {
+                        b.Remove(e);
+
+                    }
+                    b.Add(k);
+                }
+                return b;
+            }
+        }
+    }
+}
---
+++ Cxulyvattu.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thuctapdemoGiaoDien
+{
+    class Cxulyvattu
+    {
+        private databaseDataContext dc = new databaseDataContext();
+
+        public IEnumerable<object> getDSVatTu()
+        {
+            return dc.VatTus.ToList();
+        }
+        public string phatsinhma()
+        {
+            string so = "";
+            var kq = dc.VatTus.OrderByDescending(x => x.MaVT).Take(1);
+            if (kq.Count() <= 0) so = "VT0001";
+            else
+            {
+                foreach (VatTu a in kq.ToList())
+                {
+                    string s = a.MaVT.Substring(2);
+                    int n = int.Parse(s) + 1;
+                    so = "VT" + n.ToString("D4");
+
+                    break;
+                }
+            }
+            return so;
+        }
+        public void Them(VatTu a)
+        {
+            dc.VatTus.InsertOnSubmit(a);
+            dc.SubmitChanges();
+        }
+
+        public void Xoa(VatTu a)
+        {
+            dc.VatTus.DeleteOnSubmit(a);
+            dc.SubmitChanges();
+
+        }
+        public void Sua(VatTu kh)
+        {
+            foreach (VatTu a in dc.VatTus.Where(x => x.MaVT == kh.MaVT))
+            {
+                a.TenVT = kh.TenVT;
+                a.DVTinh = kh.DVTinh;
+                a.DonGia = kh.DonGia;
+                a.SoLuong = kh.SoLuong;
+                a.MaNSX = kh.MaNSX;
+                dc.SubmitChanges();
+            }
+        }
+        public VatTu Tim(VatTu kh)
+        {
+            foreach (VatTu a in dc.VatTus.Where(x => x.MaVT == kh.MaVT))
+            {
+                if (a != null)
+                    return a;
+            }
+            return null;
+        }
+        public List<VatTu> getDSVTTracuu(VatTu a,string strMa,string strTen)
+        {
+            List<VatTu> b = new List<VatTu>();
+            if (strTen == "")
+            {
+                foreach (VatTu r in dc.VatTus.Where(x => x.MaVT.Contains(strMa)))
+                {
+
+                    VatTu e = b.Find(x => x.MaVT == r.MaVT);
+                    if (e == null)
+                    {
+
+                        b.Add(r);
+
+                    }
+                }
+                return b;
+            }
+            else if (strMa == "")
+            {
+                foreach (VatTu k in dc.VatTus.Where(x => x.TenVT.Contains(strTen)))
+                {
+                    VatTu e = b.Find(x => x.MaVT == k.MaVT);
+                    if (e != null)
+                    {
+                        b.Remove(e);
+
+                    }
+                    b.Add(k);
+                }
+                return b;
+            }
+            else
+            {
+                foreach (VatTu r in dc.VatTus.Where(x => x.MaVT.Contains(strMa)))
+                {
+
+                    VatTu e = b.Find(x => x.MaVT == r.MaVT);
+                    if (e == null)
+                    {
+
+                        b.Add(r);
+
+                    }
+                }
+                foreach (VatTu k in dc.VatTus.Where(x => x.TenVT.Contains(strTen)))
+                {
+                    VatTu e = b.Find(x => x.MaVT == k.MaVT);
+                    if (e != null)
+                    {
+                        b.Remove(e);
+
+                    }
+                    b.Add(k);
+                }
+                return b;
+            }
+        }
+    }
+}
